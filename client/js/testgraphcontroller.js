05ea3b555ce9714e9373cf4d6ad214ee55da2988
Andrew Schlaline <aschlali@uci.edu>
2016-04-13 15:20:20 -0700

    made graph simulate ajax requests from a server and update the graph dynamically

--- client/js/testgraphcontroller.js
+++ client/js/testgraphcontroller.js
@@ -1,23 +1,37 @@
-app.controller('testgraph',['$scope','$window','$http',function($scope,$window,$http){
-	$scope.data =[
-            ['data1', 30, 200, 100, 400, 150, 250],
-            ['data2', 50, 20, 10, 40, 15, 25]
-        ];
-	$http({
-		url:'jsontestdata.json',
-		method: 'GET'
-	}).then(function successCallback(response){
-		$scope.data = response.data;
-		console.log($scope.data);
-		$scope.chart();
+app.controller('testgraph',['$scope','$window','$http','$timeout',function($scope,$window,$http,$timeout){
+	$scope.data =[];
 		
-	});
+	$scope.getchart = function(input_url){
+		$http({
+			url:input_url,
+			method: 'GET'
+		}).then(function successCallback(response){
+			$scope.data = response.data;
+			console.log($scope.data);
+			$scope.updatechart();
+		});
+	}
 	
-	$scope.chart = function(){
-		$window.c3.generate({
+	$scope.chart = $window.c3.generate({
 			data:{
 				json: $scope.data
 			}
 		});
+	
+	$scope.updatechart = function(){
+		$scope.chart.load({
+			json: $scope.data
+			
+		});
 	};
+	//simulate json requests from a server
+	$timeout(function(){
+		$scope.getchart('jsontestdataA.json');
+	},3000);
+	$timeout(function(){
+		$scope.getchart('jsontestdataB.json');
+	},6000);
+	$timeout(function(){
+		$scope.getchart('jsontestdataC.json');
+	},9000);
 }]);
