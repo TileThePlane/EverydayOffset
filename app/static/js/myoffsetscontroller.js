d92b4eedc1423f871c83edff8ad962ef4c9399b5
Andrew Schlaline <aschlali@uci.edu>
2016-05-08 19:51:57 -0700

    Made offset 'buttons' on the side clickable and update the main view

--- app/static/js/myoffsetscontroller.js
+++ app/static/js/myoffsetscontroller.js
@@ -1,6 +1,15 @@
 app.controller('myoffsets',['$scope','$window','$http','$timeout',function($scope,$window,$http,$timeout){
+	//all data about all offsets sent from the server
 	$scope.data ={"offsets":[{"name":"hi","active":"true"}]};
-		
+	
+	//data displayed on the page
+	$scope.datacurrentlydisplayed = {};
+	
+	//set the data that is displayed on the page.
+	$scope.setcurrentlydisplayed = function(clickedoffset){
+		$scope.datacurrentlydisplayed = clickedoffset;
+	}
+	
 	$scope.getoffsets = function(input_url){
 		$http({
 			url:input_url,
@@ -8,6 +17,7 @@ app.controller('myoffsets',['$scope','$window','$http','$timeout',function($scop
 		}).then(function successCallback(response){
 			$scope.data = response.data;
 			console.log($scope.data);
+			$scope.datacurrentlydisplayed = $scope.data.offsets.offset2;
 		});
 	};
 	//json requests from a server
