b4c5df21cd3f82768885bbb810a5823c4d55785c
Kyle Vonderwerth <kvonder@uci.edu>
2016-05-19 15:48:41 -0700

    e

--- app/myoffsets/calls.py
+++ app/myoffsets/calls.py
@@ -24,36 +24,37 @@ def myoffsets_myoffset(offset_id = ''):
     Let me know what your POST will look like
     '''
     #print(db.offsets.find_one({'offset_id' : offset_id}))
-    try:
-        if request.method == 'GET':
-            if not offset_id:
-                return jsonify({'status': status.EMPTY_400.update({'request_key' : 'offset_id'})})
-            if offset_id == 'new':
-                '''add in Andrews deets from API plan'''
-                pass
-            offset = db.offsets.find_one({'offset_id' : offset_id})
-            if offset:
-                del offset['_id']
-                npo_list = []
-                donation_amount_list = []
-                time_stamp_list = []
-                for event in offset['offset_events']:
-                    npo_list.append(event['npo'])
-                    donation_amount_list.append(event['donation_amount'])
-                    time_stamp_list.append(event['date_time_stamp'])
-                del offset['offset_events'] 
-                offset['npo_list'] = npo_list
-                offset['donation_amount_list'] = donation_amount_list
-                offset['date_time_stamp_list'] = time_stamp_list
-                return jsonify({'status' : status.STANDARD_200,
-                                'offset' : offset})
-            return jsonify({'status':status.STANDARD_404.update({'request_key' : 'offset_id',
-                                                       'request_value' : offset_id})})
-        
-        elif request.method == 'POST':
+    #try:
+    if request.method == 'GET':
+        if not offset_id:
+            return jsonify({'status': status.EMPTY_400.update({'request_key' : 'offset_id'})})
+        if offset_id == 'new':
+            '''add in Andrews deets from API plan'''
             pass
+        offset = db.offsets.find_one({'offset_id' : offset_id})
+        if offset:
+            del offset['_id']
+            npo_list = []
+            donation_amount_list = []
+            time_stamp_list = []
+            for event in offset['offset_events']:
+                npo_list.append(event['npo'])
+                donation_amount_list.append(event['donation_amount'])
+                time_stamp_list.append(event["data_time_stamp"])
+            del offset['offset_events'] 
+            offset['npo_list'] = npo_list
+            offset['donation_amount_list'] = donation_amount_list
+            offset['date_time_stamp_list'] = time_stamp_list
+            return jsonify({'status' : status.STANDARD_200,
+                            'offset' : offset})
+        return jsonify({'status':status.STANDARD_404.update({'request_key' : 'offset_id',
+                                                   'request_value' : offset_id})})
+    
+    elif request.method == 'POST':
+        pass
+        '''
     except KeyError as e:
         print(e)
         return jsonify({'status': status.EMPTY_400.update({'request_key' : 'offset_id'})})
-    
+    '''
     
